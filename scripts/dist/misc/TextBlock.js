0af95ebc491456f59dd011e7e01fae605418d584
Walorson <zembakii@gmail.com>
2024-04-02 16:19:48 +0200

    textBlock textarea added

--- scripts/dist/misc/TextBlock.js
+++ scripts/dist/misc/TextBlock.js
@@ -2,6 +2,7 @@ class TextBlock extends Block {
     constructor(x = 0, y = 0) {
         super(x, y);
         this.text = "text";
+        this.textHTML = "text";
         this.maxConnects = 0;
         this.init();
     }
@@ -9,16 +10,34 @@ class TextBlock extends Block {
         workspace.innerHTML += `<div class="block text" id="${this.id}">Sample Text</div>`;
     }
     updateDiv() {
-        this.div.innerHTML = `<span>${this.text}</span>`;
+        this.div.innerHTML = `<span>${this.textHTML}</span>`;
     }
     properties() {
         this.div.addEventListener("mousedown", () => {
             propertiesWindow.innerHTML = `
-                <p>Message: <input type="text" value="${this.text}" class="property${this.id}"></p>
+                <p>Message: <textarea class="property${this.id}">${this.text}</textarea></p>
             `;
             const property = propertiesWindow.querySelectorAll(".property" + this.id);
             property[0].oninput = () => {
                 this.text = property[0].value;
+                const indexArray = [];
+                let index = this.text.indexOf("\n");
+                let newText = "";
+                while (index != -1) {
+                    indexArray.push(index);
+                    index = this.text.indexOf("\n", index + 1);
+                }
+                for (let i = 0; i < indexArray.length; i++) {
+                    if (i == 0)
+                        newText += this.text.slice(0, indexArray[i]) + "<br>";
+                    else
+                        newText += this.text.slice(indexArray[i - 1] + 1, indexArray[i]) + "<br>";
+                }
+                newText += this.text.slice(indexArray[indexArray.length - 1] + 1, this.text.length);
+                if (indexArray.length > 0)
+                    this.textHTML = newText;
+                else
+                    this.textHTML = this.text;
                 this.updateDiv();
             };
             super.properties();
--- scripts/src/misc/TextBlock.ts
+++ scripts/src/misc/TextBlock.ts
@@ -1,6 +1,7 @@
 class TextBlock extends Block
 {
     text: string = "text";
+    textHTML: string = "text";
     maxConnects: number = 0;
     constructor(x: number = 0, y: number = 0)
     {
@@ -15,20 +16,44 @@ class TextBlock extends Block
     }
 
     updateDiv(): void {
-        this.div.innerHTML = `<span>${this.text}</span>`;
+        this.div.innerHTML = `<span>${this.textHTML}</span>`;
     }
 
     properties(): void {
     
         this.div.addEventListener("mousedown", () => {
             propertiesWindow.innerHTML = `
-                <p>Message: <input type="text" value="${this.text}" class="property${this.id}"></p>
+                <p>Message: <textarea class="property${this.id}">${this.text}</textarea></p>
             `;
 
             const property: any = propertiesWindow.querySelectorAll(".property"+this.id);
 
             property[0].oninput = () => {
                 this.text = property[0].value;
+                
+                const indexArray: number[] = [];
+                let index: number = this.text.indexOf("\n");
+                let newText: string = "";
+
+                while(index != -1) {
+                    indexArray.push(index);
+                    index = this.text.indexOf("\n", index + 1);
+                }
+
+                for(let i=0; i < indexArray.length; i++)
+                {
+                    if(i == 0)
+                        newText += this.text.slice(0, indexArray[i]) + "<br>";
+                    else
+                        newText += this.text.slice(indexArray[i-1] + 1, indexArray[i]) + "<br>";
+                }
+                newText += this.text.slice(indexArray[indexArray.length-1] + 1, this.text.length);
+
+                if(indexArray.length > 0)
+                    this.textHTML = newText;
+                else
+                    this.textHTML = this.text;
+
                 this.updateDiv();
             };  
 
